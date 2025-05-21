37602d63e4c2b83fd06f8981b88f543c9df16f1f
Coral Marie <coraltribble@gmail.com>
2025-05-21 13:15:50 -0400

    date change from string to int

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ app.get('/', (req, res) => {
 // JSON data route for /movies URL
 app.get('/movies', (req, res) => {
   res.json([
-    { title: 'Die Hard', year: '1988', actors: ['Bruce Willis'], genre: ['Action', 'Thriller'] },
-    { title: 'Mad Max: Fury Road', year: '2015', actors: ['Tom Hardy', 'Charlize Theron'], genre: ['Action', 'Adventure', 'Sci-Fi'] },
-    { title: 'John Wick', year: '2014', actors: ['Keanu Reeves'], genre: ['Action', 'Thriller', 'Crime'] },
-    { title: 'The Dark Knight', year: '2008', actors: ['Christian Bale', 'Heath Ledger'], genre: ['Action', 'Crime', 'Drama'] },
-    { title: 'Gladiator', year: '2000', actors: ['Russell Crowe'], genre: ['Action', 'Drama'] },
-    { title: 'The Raid: Redemption', year: '2011', actors: ['Iko Uwais'], genre: ['Action', 'Crime', 'Thriller'] },
-    { title: 'The Matrix', year: '1999', actors: ['Keanu Reeves', 'Carrie-Anne Moss'], genre: ['Action', 'Sci-Fi'] },
-    { title: 'Terminator 2: Judgment Day', year: '1991', actors: ['Arnold Schwarzenegger'], genre: ['Action', 'Sci-Fi'] },
-    { title: 'Inception', year: '2010', actors: ['Leonardo DiCaprio'], genre: ['Action', 'Sci-Fi', 'Thriller'] },
-    { title: 'James Bond: Skyfall', year: '2012', actors: ['Daniel Craig'], genre: ['Action', 'Thriller', 'Spy'] }
+    { title: 'Die Hard', year: 1988, actors: ['Bruce Willis'], genre: ['Action', 'Thriller'] },
+    { title: 'Mad Max: Fury Road', year: 2015, actors: ['Tom Hardy', 'Charlize Theron'], genre: ['Action', 'Adventure', 'Sci-Fi'] },
+    { title: 'John Wick', year: 2014, actors: ['Keanu Reeves'], genre: ['Action', 'Thriller', 'Crime'] },
+    { title: 'The Dark Knight', year: 2008, actors: ['Christian Bale', 'Heath Ledger'], genre: ['Action', 'Crime', 'Drama'] },
+    { title: 'Gladiator', year: 2000, actors: ['Russell Crowe'], genre: ['Action', 'Drama'] },
+    { title: 'The Raid: Redemption', year: 2011, actors: ['Iko Uwais'], genre: ['Action', 'Crime', 'Thriller'] },
+    { title: 'The Matrix', year: 1999, actors: ['Keanu Reeves', 'Carrie-Anne Moss'], genre: ['Action', 'Sci-Fi'] },
+    { title: 'Terminator 2: Judgment Day', year: 1991, actors: ['Arnold Schwarzenegger'], genre: ['Action', 'Sci-Fi'] },
+    { title: 'Inception', year: 2010, actors: ['Leonardo DiCaprio'], genre: ['Action', 'Sci-Fi', 'Thriller'] },
+    { title: 'James Bond: Skyfall', year: 2012, actors: ['Daniel Craig'], genre: ['Action', 'Thriller', 'Spy'] }
   ]);
 });
 
